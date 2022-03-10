ebe2a3b4723c6a5057796f598e8dab4dafdfda2a
guilherme eduardo <guilherme09112001@gmail.com>
2022-03-09 23:03:34 -0300

    Add a simple form to edit view

--- src/screens/Edit/index.js
+++ src/screens/Edit/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { Text, View, TouchableOpacity} from 'react-native';
+import { Text, View, TouchableOpacity, TextInput } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import styles from './styles';
 import MenuAreaRestrita from '../../components/MenuAreaRestrita';
 
-const Profile = ({navigation}) => {
-    
-    return(
-        <View>
+const Profile = ({ navigation }) => {
+
+    return (
+        <View style={[styles.fullHeight, styles.backgroundWhite]}>
             <MenuAreaRestrita title="Editar" navigation={navigation} />
+
+            <View style={styles.container}>
+                <View style={styles.width}>
+
+                    <TextInput
+                        placeholder='Nome do produto'
+                        style={styles.inputGray}
+                    />
+                    <TextInput
+                        placeholder='Código do produto'
+                        style={styles.inputGray}
+                    />
+
+                </View>
+            </View>
         </View>
-    )};
+    )
+};
 export default Profile;
--- src/screens/Edit/styles.js
+++ src/screens/Edit/styles.js
@@ -1,23 +1,9 @@
 import { StyleSheet } from "react-native";
+import {global, colors} from "../../assets/css";
 
 const styles = StyleSheet.create({  
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 10
-    },
-    TouchableOpacity: {
-        width: '60%',
-        backgroundColor: '#424242',
-        padding: 15,
-        marginBottom: 15,
-        borderRadius: 10
-    },
-    textWhite: {
-        color: '#fff',
-        textAlign: 'center'
-    }
+    ...global,
+    
+    
 });
 export default styles;
