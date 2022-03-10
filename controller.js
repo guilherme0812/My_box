a5b922f489bcee66139b5bde7921fa8f2047fb33
guilherme eduardo <guilherme09112001@gmail.com>
2022-03-09 22:22:03 -0300

    add btn share qrcode

--- controller.js
+++ controller.js
@@ -2,11 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const models = require('./models');
+const QRCode = require('qrcode')
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
+// Permitir que o usuário acesse o assets
+app.use(express.static('assets'));
+
+// Modulos
 let user = models.User;
 let tracking = models.Tracking;
 let product = models.Product;
@@ -14,7 +19,7 @@ let product = models.Product;
 
 app.post('/login', async (req, res) => {
   let response = await user.findOne({
-    where: {name: req.body.name, password: req.body.password}
+    where: { name: req.body.name, password: req.body.password }
   })
   if (response === null) {
     res.send(JSON.stringify('error'))
@@ -25,43 +30,51 @@ app.post('/login', async (req, res) => {
 
 app.post('/verifyPass', async (req, res) => {
   let response = await user.findOne({
-    where: {id: req.body.id, password:req.body.senhaAntiga}
+    where: { id: req.body.id, password: req.body.senhaAntiga }
   })
   if (response === null) {
     res.send(JSON.stringify("A senha antiga não confere."))
   } else {
-     // Verificar se as senhas foram digitadas iguais
-     if (req.body.novaSenha === req.body.confNovaSenha) {
-       // Atualizando password no banco de dados
-       response.password = req.body.novaSenha
-       response.save()
-       res.send(JSON.stringify('Senha atualizada com sucesso!'))
-     } else {
-       res.send(JSON.stringify('As senhas nãp conferem'))
-     }
+    // Verificar se as senhas foram digitadas iguais
+    if (req.body.novaSenha === req.body.confNovaSenha) {
+      // Atualizando password no banco de dados
+      response.password = req.body.novaSenha
+      response.save()
+      res.send(JSON.stringify('Senha atualizada com sucesso!'))
+    } else {
+      res.send(JSON.stringify('As senhas nãp conferem'))
+    }
   }
 })
 
 // Cadastrar produto no banco de dados
 app.post('/create', async (req, res) => {
-    let trackingId = ''
-    let create = await tracking.create({
-      code: req.body.code,
-      local: req.body.local, 
-      userId: req.body.userId,
-      createdAt: new Date(), 
-      updatedAt: new Date()
-   }).then( (response)=>{
-     trackingId += response.id
-   })
+  let trackingId = ''
+  let create = await tracking.create({
+    code: req.body.code,
+    local: req.body.local,
+    userId: req.body.userId,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }).then((response) => {
+    trackingId += response.id
+  })
 
-   await product.create({
-     trackingId: trackingId,
-     name: req.body.product
-   })
+  await product.create({
+    trackingId: trackingId,
+    name: req.body.product
+  })
+
+  QRCode.toDataURL(req.body.code).then(url => {
+    QRCode.toFile(
+      './assets/img/code.png',
+      req.body.code
+    )
+    res.send(JSON.stringify(url))
+  })
 })
 
 let port = process.env.PORT || 3000;
-app.listen(port, (req,res)=> {
-    console.log("Servidor rodando")
+app.listen(port, (req, res) => {
+  console.log("Servidor rodando")
 })
--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,26 +1,29 @@
-import React, {useState,useEffect} from 'react';
-import { Text, View, TouchableOpacity, TextInput} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, TouchableOpacity, TextInput, Image, Button } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import MenuAreaRestrita from '../../components/MenuAreaRestrita';
 import config from '../../../config/config'
 import styles from './styles';
+import * as FileSystem from 'expo-file-system';
+import * as Sharing from 'expo-sharing';
 
-const Register = ({navigation}) => {
-    
+const Register = ({ navigation }) => {
+
     const address = config.origin
     const [code, setCode] = useState(null)
     const [user, setUser] = useState(null)
     const [product, setProduct] = useState(null)
     const [response, setResponse] = useState(null)
 
-    useEffect( () =>{
+    useEffect(() => {
         getIdUser()
-    },[])
+    }, [])
 
-    useEffect( () =>{
+    useEffect(() => {
         randomCode()
-    },[])
+        setProduct(null)
+    }, [response])
 
     // Pegar id do usuário
     async function getIdUser() {
@@ -30,19 +33,19 @@ const Register = ({navigation}) => {
     }
 
     //Gerar um código randômico
-    async function randomCode(){
+    async function randomCode() {
         let result = '';
-        let length=20;
-        let chars='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        let length = 20;
+        let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
         setCode(result);
     }
 
     // Envio do formulário
     async function sendForm() {
-        let response = await fetch(`${config.urlRoot}create`,{
+        let response = await fetch(`${config.urlRoot}create`, {
             method: 'post',
-            headers:{
+            headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json'
             },
@@ -57,19 +60,45 @@ const Register = ({navigation}) => {
         setResponse(json)
     }
 
-    return(
+    //Compartilhar o QRCode
+    async function shareQR() {
+        const image = config.urlRoot + 'img/code.png';
+        FileSystem.downloadAsync(
+            image,
+            FileSystem.documentDirectory + '.png'
+        ).then(({ uri }) => {
+            Sharing.shareAsync(uri);
+        });
+        await Sharing.shareAsync();
+    }
+
+    return (
         <View style={[styles.fullHeight, styles.backgroundWhite]}>
             <MenuAreaRestrita title="Registrar" navigation={navigation} />
 
             <View style={styles.container}>
                 <View style={styles.width}>
-                    
-                    <TextInput  placeholder='Registrar' style={styles.inputGray} onChangeText={text => {setProduct(text)}} />
-                    <TouchableOpacity style={styles.button}  onPress={()=>sendForm()} >
+
+                    {response && (
+                        <View>
+
+                            <Image source={{ uri: response, height: 180, width: 180 }} />
+                            <Button title='Compartilhar' onPress={() => shareQR()} />
+                        </View>
+                    )}
+
+                    <TextInput
+                        placeholder='Registrar'
+                        style={styles.inputGray}
+                        onChangeText={text => { setProduct(text) }}
+                        value={product}
+                    />
+                    <TouchableOpacity style={styles.button} onPress={() => sendForm()} >
                         <Text style={styles.textButton}>Enviar</Text>
                     </TouchableOpacity>
                 </View>
             </View>
         </View>
-    )};
+    )
+};
 export default Register;
